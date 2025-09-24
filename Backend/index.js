e192345a9adcb1d2962b8f82f37c69e9f088b36b
htom <hatvanitamas09@gmail.com>
2025-09-24 15:05:41 +0200

    added login endpoint, now need to create db

--- Backend/index.js
+++ Backend/index.js
@@ -22,7 +22,26 @@ app.get('/', (req, res) => {
     res.send("Hello from Habit Tracker");
 })
 
+app.get('/login', async (req, res) => {
+    let connection
 
+    try {
+        connection = await getConnection();
+        const { username, password } = req.body
+        const [rows] = await connection.query('select * from user where username=' + username)
+
+        if (username == rows.username && password == rows.password) {
+            res.status(200).json({ text: "Success" })
+        } else {
+            res.status(401).json({ text: "Unauthorized" })
+        }
+    } catch (error) {
+        console.log("Error occured in /login", error)
+        res.status(400).json({ text: "Bad Request" })
+    } finally {
+        if (connection) await connection.end()
+    }
+})
 
 app.listen(PORT, () => {
     console.log(`Running Habit Tracker backend on port: ${PORT}`);

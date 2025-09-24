9219d41a5421c4c72e99b43c6100850961bb7c5d
htom <hatvanitamas09@gmail.com>
2025-09-24 14:29:36 +0200

    added async conn function to the backend

--- Backend/index.js
+++ Backend/index.js
@@ -8,6 +8,22 @@ const PORT = 9670;
 app.use(cors());
 app.use(express.json());
 
+
+const getConnection = async () => {
+    return await mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'HabitTracker',
+    });
+};
+
 app.get('/', (req, res) => {
     res.send("Hello from Habit Tracker");
+})
+
+
+
+app.listen(PORT, () => {
+    console.log(`Running Habit Tracker backend on port: ${PORT}`);
 })
